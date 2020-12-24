03264215a5aebafc912246ac444a47b7517fe144
shadifadila <shadifadila@gmail.com>
2020-12-24 09:54:31 -0800

    v3

---
+++ README.d
@@ -0,0 +1,10 @@
+# Spring Kubernestes project
+### in this task we build a docker image for spring-website that include a database (mysql server)
+- step 1: 
+    - we create a DockerFile 
+    - we build the image using this command "docker build . -t shadifadila/my_repo:spring"
+    - we upload the image to dokcerhub : 
+        - the avaible on this path : 
+            shadifadila/my_repo:spring
+- step 2 : 
+    
